f6c0dad5062e0c1bd8312d9fcb026b2e4776b08e
khoathichsanban <93322085+khoathichsanban@users.noreply.github.com>
2024-03-29 21:27:30 +0700

    Update 08.js

--- Homework08/08.js
+++ Homework08/08.js
@@ -1,23 +1,19 @@
 let arr = [1, 5, 6, 3, 2, 1, 1, 5, 6, 7];
-let sum = parseInt(prompt("Enter a number: "));
 let newArr = [];
 let isExist = false;
 for (let i = 0; i < arr.length; i++) {
   newArr.push(arr[i]);
-  let total = 0;
+  let count = 0;
   for (let j = 0; j < newArr.length; j++) {
-    total += newArr[j];
+    if (arr[i] === newArr[j]) {
+      count++;
+    }
   }
-  if (total === sum) {
-    console.log(newArr);
+  if (count > 1) {
+    console.log(arr[i]);
     isExist = true;
-    break;
-  }
-  if (total > sum) {
-    newArr.shift();
-    i--;
   }
 }
 if (!isExist) {
-  console.log("Không có mảng con thỏa mãn");
+  console.log("Không có phần tử trùng lặp");
 }
